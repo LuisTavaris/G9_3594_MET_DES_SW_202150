513657b968edd22f6f292f9014d5a0bbf0f244c3
luistavaris <luistavaris@hotmail.es>
2021-08-24 22:59:01 -0500

    Codigo V_0.4

---
+++ codigoFuente/simulatorSystem_V0.4/src/ec/edu/espe/simulatorSystem/model/Usuario.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.simulatorSystem.model;
+
+
+/**
+ *
+ * @author luist
+ */
+public class Usuario extends PersonalEmpresarial {
+    
+      private String usuario;
+      private String contraseña;
+      private String IDEmpresarial;
+      
+      //CONSTRUCTOR
+    
+    public Usuario(String nombres, String apellidos, String ID, String dateOfBirth, String numeroCelular, String direccion, String correo, boolean option) {
+        super(nombres, apellidos, ID, dateOfBirth, numeroCelular, direccion, correo, option);
+      
+    }
+
+    public Usuario(String usuario, String contraseña,  boolean option) {
+        super(option);
+        this.usuario = usuario;
+        this.contraseña = contraseña;
+      
+    }
+
+    
+    // SETTER AND GETTER
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public String getIDEmpresarial() {
+        return IDEmpresarial;
+    }
+
+    public void setIDEmpresarial(String IDEmpresarial) {
+        this.IDEmpresarial = IDEmpresarial;
+    }
+    
+    
+}
